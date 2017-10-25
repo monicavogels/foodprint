815caa255ec241ce6368c2c0d49d5701f1b1a0b6
monicavogels <monicavogels@live.nl>
2017-10-25 17:15:08 +0200

    Entry Page + Style

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
-console.log("jQuery is active.");
+	
+	console.log("jQuery is active.");
+	
 
+	// items are set on display: none; but if hide === false, make them show
+	var hide = localStorage.getItem('hide');
+	if(hide === 'true'){
+		$('#personal_login, #personal_popup').css('display', 'none');
+	} else {
+		$('#personal_login, #personal_popup').css('display', 'flex');
+	}
+
+	// if you click the button, hide this and show that.
+	$('#personal_login button').click(function() {
+		event.preventDefault();
+	    $('#personal_login, #personal_popup').css('display', 'none');
+	    localStorage.setItem('hide', 'true');
+	});
+
+	// unset items in local storage
+	$('.logout').click(function() {
+		localStorage.removeItem('hide');
+	});
 
 });
